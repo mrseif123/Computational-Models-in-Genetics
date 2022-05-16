844b50b5751b910d94bda0e2ad4d0afa9e5bc8e8
Seaf Aliyan <codo47@gmail.com>
2022-05-16 12:39:39 +0300

    added enviroment setup

--- assignment2.py
+++ assignment2.py
@@ -3,17 +3,57 @@
 # By: Seaf Aliyan, mrseif123.
 
 ## Question 1, NK Models:-
-def generate_binary_language_helper(N, part, lst):
-    if N - 1:
-        generate_binary_language_helper(N - 1, part + '0', lst)
-        generate_binary_language_helper(N - 1, part + '1', lst)
-    else:
-        lst.append('1' + part)
+def process_number(n_as_lst):
+    s = ""
+    for i in n_as_lst:
+        s = s + str(i)
+    return s
 
 
-def generate_binary_language(lst=[], N=14):
-    generate_binary_language_helper(N, '', lst)
-    return lst
+def generateAllBinaryStrings(n, arr, i, bin_lst):
+    if i == n:
+        bin_lst.append(process_number(arr[0:n]))
+        return
+
+    arr[i] = 0
+    generateAllBinaryStrings(n, arr, i + 1, bin_lst)
+
+    arr[i] = 1
+    generateAllBinaryStrings(n, arr, i + 1, bin_lst)
+
+
+def isPowerOfTwo(x):
+    # First x in the below expression is
+    # for the case when x is 0
+    return x and (not (x & (x - 1)))
+
+
+# function to check whether the two numbers
+# differ at one bit position only
+def differAtOneBitPos(a, b):
+    return isPowerOfTwo(a ^ b)
+
+
+def dec_rep(n):
+    return int(n, 2)
+
+
+def get_decimal_representation(lst):
+    decimal_list = list(map(dec_rep, lst))
+    return decimal_list
+
+
+def get_neighbours_map(bin_lst, neighbours_map):
+    normal_list = get_decimal_representation(bin_lst)
+    for binary in bin_lst:
+        neighbours_map[binary] = []
+
+    for a in normal_list:
+        for b in normal_list:
+            if a != b and differAtOneBitPos(a, b):
+                tmp_lst = neighbours_map[bin_lst[a]]
+                tmp_lst.append(bin_lst[b])
+                neighbours_map[bin_lst[a]] = tmp_lst
 
 
 def simulate_NK(N=14, K=[0, 4, 10]):
@@ -21,5 +61,12 @@ def simulate_NK(N=14, K=[0, 4, 10]):
 
 
 if __name__ == '__main__':
-    bin_list = generate_binary_language(lst=[], N=14)
-    print(bin_list)
+    # Question 1:
+    # Setting up environment:-
+    N = 4
+    helper_list = [None] * N
+    bin_lst = []
+    neighbours_map = {}
+    generateAllBinaryStrings(N, helper_list, 0, bin_lst)
+    get_neighbours_map(bin_lst, neighbours_map)
+    print(neighbours_map)
